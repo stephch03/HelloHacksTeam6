ab15cbe30d9245b154cd8250e901dd87996f53f9
stephch03 <114316577+stephch03@users.noreply.github.com>
2022-09-25 14:17:05 -0700

    FinalVersion

--- dataframe.py
+++ dataframe.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 df2 = {'name': ['jay', 'mich', 'step', 'sana'],
-'email': ["jayp", "mich@gmail.com", "stephanie@gmail.com", "sanana@gmail.com"],
-'password': ["hi","yo","halla","hacks"],
+'email': ["jayp@gmail.com", "mich@gmail.com", "stephanie@gmail.com", "sanana@gmail.com"],
+'password': ["hellohacks","michw0821","hello","hacks"],
     }
 df_marks = pd.DataFrame(df2)
 print('Original DataFrame\n------------------')
-print(df_marks)
+#print(df_marks)
 #df_marks.to_csv("dataFrame.csv")
 
 
@@ -18,40 +18,15 @@ def addUser(un,em,pw):
     #print(finaldf)
     finaldf.to_csv("dataFrame.csv")
 
-addUser("benny","sana@gmail.com","sananananana")
+#addUser("benny","sana@gmail.com","sananananana")
+#def getUserInfo(un):
+#    for i in range(len(df2)):
+#        print(df2.loc[i, "name"], df.loc[i, "email"])
 
-def getUserInfo(un):
-    df_marks.dropna(inplace = True)
-    start = 0
-    df_marks["Indexes"]= df_marks["name"].str.find(un, start)
-    print(df_marks["name"].where(df2["Indexes"] > -1))
-    print(df_marks["e-mail"].where(df2["Indexes"] > -1))
-    print(df_marks["password"].where(df2["Indexes"] > -1))
-    return df_marks
+#getUserInfo('jay')
 
-#print(df2)
-print(getUserInfo('jay'))
+#def csvtojson():
+#    csvcheck = pd.read_csv('dataframe.csv')
+#    csvcheck.to_json(r'dataBase.json')
 
-
-
-
-#entry = [df2,"michelle","michelle@gmail.com", "mcpassword"]
-#pd.concat(entry)
-
-#import csv
-#import json
- 
-#def make_json(csvFilePath, jsonFilePath):
-#    data = {}
-#    with open(csvFilePath, encoding='utf-8') as csvf:
-#       csvReader = csv.DictReader(csvf)
-#        for rows in csvReader:
-#            key = rows['No']
-#            data[key] = rows
-
-#    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-#        jsonf.write(json.dumps(data, indent=4))
-
-#csvFilePath = 'dataFrame.csv'
-#jsonFilePath = 'dataBase.json'
-#make_json("dataFrame.csv", "dataBase.json")
+#csvtojson()
