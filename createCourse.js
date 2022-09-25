9128e7f33ee81e4f4f02e6269a39b51e6484ad3d
jpkani <jaybinpark@gmail.com>
2022-09-24 18:33:50 -0700

    commit and sync

---
+++ createCourse.js
@@ -0,0 +1,13 @@
+import { people } from "./video";
+
+
+export function createCourse(input) {
+    const courseExists = people.find((class { })); {
+        return class { }.courseName === input.coursename;
+    };
+    const newCourse = {
+        courseName: input.courseName,
+    };
+    people.push(newCourse);
+}
+
--- video.js
+++ video.js
@@ -1,80 +1,78 @@
 import data from "./data.json" assert {type: 'json'};
-let people = data;
+export let people = data;
 
-    export function createCourse(input){
-        const courseExists = people.find((class) => {
-            return class.courseName === input.coursename
-        });
+export function createCourse(input) {
+    const courseExists = people.find((course) => {
+        return course.courseName === input.courseName;
+    });
+    const newCourse = {
+        courseName: input.courseName,
+    };
 
-
-    }
-
-    
-    
-
-
+    people.push(newCourse);
+}
 
 export function createPeople(input) {
-  const idExists = people.find((person) => {
-    return person.name === input.name;
-  }); // We check if a user already exists with the same name; because we determined the name to be the id, we do not want to create an entry with a duplicate id.
-  if (idExists) {
-    return people; // You could return an error here instead; this would be a design choice!
-  }
-  const newPerson = {
-    name: input.name,
-    // Ternary expression syntax: "?" means "if", ":" means "else"
-    interests: input.interests ? input.interests.split(",") : [],
-    subscribed: Boolean(input.subscribed),
-    demographics: {
-      country: input.country,
-      age: Number(input.age)
+    const idExists = people.find((person) => {
+        return person.name === input.name;
+    }); // We check if a user already exists with the same name; because we determined the name to be the id, we do not want to create an entry with a duplicate id.
+    if (idExists) {
+        return people; // You could return an error here instead; this would be a design choice!
     }
-  };
-  people.push(newPerson);
-  return people;
+    const newPerson = {
+        name: input.name,
+        // Ternary expression syntax: "?" means "if", ":" means "else"
+        interests: input.interests ? input.interests.split(",") : [],
+        subscribed: Boolean(input.subscribed),
+        demographics: {
+            country: input.country,
+            age: Number(input.age)
+        }
+    };
+    people.push(newPerson);
+    return people;
 }
 
 export function readPeople(input) {
-  let result = people;
-  if (input.name) {
-    result = result.filter((person) => {
-      return person.name === input.name;
-    });
-  }
-  if (input.country) {
-    result = result.filter((person) => {
-      return person.demographics.country === input.country;
-    });
-  }
-  return result;
+    let result = people;
+    if (input.name) {
+        result = result.filter((person) => {
+            return person.name === input.name;
+        });
+    }
+    if (input.country) {
+        result = result.filter((person) => {
+            return person.demographics.country === input.country;
+        });
+    }
+    return result;
 }
 
 export function updatePeople(input) {
-  const index = people.findIndex((person) => {
-    return person.name === input.name;
-  });
-  if (index === -1) {
+    const index = people.findIndex((person) => {
+        return person.name === input.name;
+    });
+    if (index === -1) {
+        return people;
+    }
+    const person = people[index];
+
+    person.subscribed = Boolean(input.subscribed);
+    if (input.interests) {
+        const interestsArray = input.interests.split(",");
+        person.interests = person.interests.concat(interestsArray);
+    }
+    if (input.age) {
+        person.demographics.age = Number(input.age);
+    }
+
+    people[index] = person;
     return people;
-  }
-  const person = people[index];
-
-  person.subscribed = Boolean(input.subscribed);
-  if (input.interests) {
-    const interestsArray = input.interests.split(",");
-    person.interests = person.interests.concat(interestsArray);
-  }
-  if (input.age) {
-    person.demographics.age = Number(input.age);
-  }
-
-  people[index] = person;
-  return people;
 }
 
 export function deletePeople(input) {
-  people = people.filter((person) => {
-    return person.name !== input.name;
-  });
-  return people;
+    people = people.filter((person) => {
+        return person.name !== input.name;
+    });
+    return people;
 }
